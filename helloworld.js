12d0f3e7d597bd238fe1febbb1f9d7ffd7e55087
prakash <bhanu@gmail.com>
2024-08-20 08:07:08 +0530

    first commit

---
+++ helloworld.js
@@ -0,0 +1,6 @@
+
+var refh1 = document.createElement("h1");
+refh1.innerHTML="This HTML Element is Created By Using JS";
+
+var refbody = document.getElementById("one");
+refbody.appendChild(refh1);
